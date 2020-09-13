7c297b334cff273efecba949df4b7700293eaa60
Ink3988 <44293641+Ink3988@users.noreply.github.com>
2020-09-13 20:38:07 +0100

    rewrite part 1

--- cleanup.py
+++ cleanup.py
@@ -1,3 +1,27 @@
-import shutil
-shutil.rmtree("working_files/cropped_frames", ignore_errors=True)
-shutil.rmtree("working_files/split_frames", ignore_errors=True)
+import os
+import numpy
+import cv2
+cap = cv2.VideoCapture('working_files\source_videos\DJI_0485_Trim_2.mp4')
+count = 0
+while cap.isOpened():
+    frame = cap.read()
+    h, w, c = frame.shape
+    if w % 2 == 0:
+        w1 = w/2 - 1
+        w2 = w/2
+    else:
+        w1 = w/2 - 0.5
+        w2 = w/2 + 0.5
+    if count != 0 and count != 1:
+        frameCropped = frame[0:w1, 0:w2]
+    elif frame == 1:
+        outputIMG = "working_files/output_image/output.png"
+        frameCropped = frame[0:w1, 0:w2]
+    else:
+        frame.save("working_files/output_image/output.png", format="png")
+    try: cv2.imshow('preview', outputIMG)
+    except: break
+    count += 1
+
+cap.release()
+cv2.destroyAllWindows() # destroy all opened windows
