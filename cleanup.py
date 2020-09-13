1726cc4fd3d3fd2a86e3fc765813ed3096c24822
Ink3988 <44293641+Ink3988@users.noreply.github.com>
2020-09-13 23:33:43 +0100

    rewrite part 2

--- cleanup.py
+++ cleanup.py
@@ -1,27 +1,36 @@
 import os
-import numpy
-import cv2
-cap = cv2.VideoCapture('working_files\source_videos\DJI_0485_Trim_2.mp4')
+import numpy as np
+import cv2, png
+cap = cv2.VideoCapture('working_files\source_videos\FORWEB2_1 - Copy.mp4')
+total = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 count = 0
 while cap.isOpened():
-    frame = cap.read()
-    h, w, c = frame.shape
-    if w % 2 == 0:
-        w1 = w/2 - 1
-        w2 = w/2
-    else:
-        w1 = w/2 - 0.5
-        w2 = w/2 + 0.5
-    if count != 0 and count != 1:
-        frameCropped = frame[0:w1, 0:w2]
-    elif frame == 1:
-        outputIMG = "working_files/output_image/output.png"
-        frameCropped = frame[0:w1, 0:w2]
-    else:
-        frame.save("working_files/output_image/output.png", format="png")
-    try: cv2.imshow('preview', outputIMG)
-    except: break
-    count += 1
-
+    ret, frame = cap.read()
+    try: 
+        h, w, c = frame.shape
+        if w%2 == 0: w = w/2
+        else: w = w/2 + 0.5
+        h = int(h)
+        w = int(w)
+        if count != 0 and count != 1:
+            frameCropped = frame[0:h, 0:w]
+            frameConjoined = cv2.hconcat([frameConjoined, frameCropped])
+        elif count == 1:
+            outputIMG = cv2.imread("working_files/output_image/output.png")
+            frameCropped = frame[0:h, 0:w]
+            frameConjoined = cv2.hconcat([outputIMG, frameCropped])
+        else:
+            frameCropped = frame[0:h, 0:w]
+            cv2.imwrite("working_files/output_image/output.png", frameCropped)
+        if ret is False:
+            print("done!")
+            break
+        count += 1
+        print(count, "of", total)
+    except:
+        print("an error happened but this is normal if all the frames were processed before this message appeared - please tell me if they were are not")
+        break
+try:cv2.imwrite("working_files/output_image/output.png", frameConjoined)
+except: print("the bad kind of error happened")
+print("complete")
 cap.release()
-cv2.destroyAllWindows() # destroy all opened windows
