c9521f525d843641ca8fcfe46bfff9173ccf8543
Ink3988 <44293641+Ink3988@users.noreply.github.com>
2020-09-15 11:33:19 +0100

    rewrite part idk like 4 now?

--- cleanup.py
+++ cleanup.py
@@ -4,33 +4,29 @@ import cv2
 cap = cv2.VideoCapture('working_files\source_videos\FORWEB2_1 - Copy.mp4')
 total = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 count = 0
+w = cap.get(3)
+h = cap.get(4)
+if w%2 == 0: w = w/2
+else: w = w/2 + 0.5
+w = int(w)
+h = int(h)
 while cap.isOpened():
     ret, frame = cap.read()
-    try: 
-        h, w, c = frame.shape
-        if w%2 == 0: w = w/2
-        else: w = w/2 + 0.5
-        h = int(h)
-        w = int(w)
-        if count != 0 and count != 1:
-            frameCropped = frame[0:h, 0:w]
-            frameConjoined = cv2.hconcat([frameConjoined, frameCropped])
-        elif count == 1:
-            outputIMG = cv2.imread("working_files/output_image/output.png")
-            frameCropped = frame[0:h, 0:w]
-            frameConjoined = cv2.hconcat([outputIMG, frameCropped])
-        else:
-            frameCropped = frame[0:h, 0:w]
-            cv2.imwrite("working_files/output_image/output.png", frameCropped)
-        if ret is False:
-            print("done!")
-            break
-        count += 1
-        print(count, "of", total)
-    except:
-        print("an error happened but this is normal if all the frames were processed before this message appeared - please tell me if they were are not")
+    if count != 0 and count != 1:
+        frameCropped = frame[0:h, 0:w]
+        frameConjoined = cv2.hconcat([frameConjoined, frameCropped])
+    elif count == 1:
+        outputIMG = cv2.imread("working_files/output_image/output.png")
+        frameCropped = frame[0:h, 0:w]
+        frameConjoined = cv2.hconcat([outputIMG, frameCropped])
+    else:
+        frameCropped = frame[0:h, 0:w]
+        cv2.imwrite("working_files/output_image/output.png", frameCropped)
+    if ret is False:
+        print("done!")
         break
-try:cv2.imwrite("working_files/output_image/output.png", frameConjoined)
-except: print("the bad kind of error happened")
+    count += 1
+    print(count, "of", total)   
+cv2.imwrite("working_files/output_image/output.png", frameConjoined)
 print("complete")
 cap.release()
