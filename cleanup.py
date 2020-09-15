dd75b61d112523bf97bc81439648a4670db0bec4
Ink3988 <44293641+Ink3988@users.noreply.github.com>
2020-09-15 11:56:03 +0100

    preparing for threading perhaps?

--- cleanup.py
+++ cleanup.py
@@ -4,23 +4,27 @@ import cv2
 cap = cv2.VideoCapture('working_files\source_videos\FORWEB2_1 - Copy.mp4')
 total = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 count = 0
-w = cap.get(3)
+w1 = cap.get(3)
+w2 = w1
 h = cap.get(4)
-if w%2 == 0: w = w/2
-else: w = w/2 + 0.5
-w = int(w)
+if w1%2 == 0: w1 = w1/2
+else: w = w1/2 - 0.5
+if w2%2 == 0: w2 = w2/2 + 1
+else: w = w2/2 + 0.5
+w1 = int(w1)
+w2 = int(w2)
 h = int(h)
 while cap.isOpened():
     ret, frame = cap.read()
     if count != 0 and count != 1:
-        frameCropped = frame[0:h, 0:w]
+        frameCropped = frame[w1:h, 0:w2]
         frameConjoined = cv2.hconcat([frameConjoined, frameCropped])
     elif count == 1:
         outputIMG = cv2.imread("working_files/output_image/output.png")
-        frameCropped = frame[0:h, 0:w]
+        frameCropped = frame[w1:h, 0:w2]
         frameConjoined = cv2.hconcat([outputIMG, frameCropped])
     else:
-        frameCropped = frame[0:h, 0:w]
+        frameCropped = frame[w1:h, 0:w2]
         cv2.imwrite("working_files/output_image/output.png", frameCropped)
     if ret is False:
         print("done!")
