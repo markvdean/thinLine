4ccc9ad99c00af6110418b3c3b443f4b0c062d4a
Mark Dean <44293641+markvdean@users.noreply.github.com>
2020-09-15 20:00:29 +0100

    band slice mode

--- cleanup.py
+++ cleanup.py
@@ -1,18 +1,21 @@
 import os
 import numpy as np
 import cv2
-cap = cv2.VideoCapture('working_files\source_videos\Vid.mp4')
+x = "\\" + str(input("file pls  "))
+cap = cv2.VideoCapture("working_files\\source_videos" + x)
 total = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 count = 0
-w1 = cap.get(3)
-w2 = w1
+# w1 = cap.get(3)
+# w2 = w1
+w1 = 0
+w2 = 10
 h = cap.get(4)
-if w1%2 == 0: w1 = w1/2
-else: w = w1/2 - 0.5
-if w2%2 == 0: w2 = w2/2 + 1
-else: w = w2/2 + 0.5
-w1 = int(w1)
-w2 = int(w2)
+# if w1%2 == 0: w1 = w1/2
+# else: w = w1/2 - 0.5
+# if w2%2 == 0: w2 = w2/2 + 1
+# else: w = w2/2 + 0.5
+# w1 = int(w1)
+# w2 = int(w2)
 h = int(h)
 while cap.isOpened():
     try:
@@ -20,16 +23,20 @@ while cap.isOpened():
         if count != 0 and count != 1:
             frameCropped = frame[0:h, w1:w2]
             frameConjoined = cv2.hconcat([frameConjoined, frameCropped])
+            cv2.imshow("preview", frameConjoined)
         elif count == 1:
             outputIMG = cv2.imread("working_files/output_image/output.png")
             frameCropped = frame[0:h, w1:w2]
             frameConjoined = cv2.hconcat([outputIMG, frameCropped])
+            cv2.imshow("preview", frameConjoined)
         else:
             frameCropped = frame[0:h, w1:w2]
             cv2.imwrite("working_files/output_image/output.png", frameCropped)
         if ret is False:
             print("done!")
             break
+        w1 += 10
+        w2 += 10
     except: 
         print("uh oh")
         break
