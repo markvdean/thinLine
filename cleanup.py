b2098577f99e2fabf55804c5df89c4ad0dc68603
Mark Dean <44293641+markvdean@users.noreply.github.com>
2020-09-16 23:40:05 +0100

    back to working state

--- cleanup.py
+++ cleanup.py
@@ -13,14 +13,11 @@ if input("timelapse mode? yes/no  ") == "no": # adjusts ending and beginning of 
     else: w = w1/2 - k/2
     if w2%2 == 0: w2 = w2/2 + k
     else: w = w2/2 + k/2
+    k = 0
 else:
     k = int(input("slice width? int>0  "))
     w1 = 0
     w2 = k
-if k >= 3 and int(input("blend slices? (best used in timelapse mode) yes/no  ")) == "yes": # add option for ignoring absolutes and therefore reducing minimum to 2 with adjusted version of the blending part?
-    blend = True
-    bpoint = round(float(1/k), 2)
-else: blend = False
 h = cap.get(4)
 h = int(h)
 w1 = int(w1)
@@ -31,40 +28,21 @@ while cap.isOpened():
         if count != 0 and count != 1:
             frameCropped = frame[0:h, w1:w2]
             frameConjoined = cv2.hconcat([frameConjoined, frameCropped])
-            # if count % 2 != 0: cv2.imwrite("working_files/output_image/previousframe-1.png", frameCropped)
-            # else: cv2.imwrite("working_files/output_image/previousframe-2.png", frameCropped)
         elif count == 1:
             outputIMG = cv2.imread("working_files/output_image/output.png")
             frameCropped = frame[0:h, w1:w2]
-            if blend == True:
-                cv2.imwrite("working_files/output_image/previousframe-1.png", frameCropped)
-                w3 = w1
-                w4 = w1 + 1
-                for i in range(k):
-                    frame4Blend = frame[0:h, w3:w4]
-                    # todo: split slice into pixels and blend previous frame over the top in a linear gradient starting with the previous ending with the current, ignoring absolutes for blending, adding original on the end, though.
-                    w3 += 1
-                    w4 += 1
             frameConjoined = cv2.hconcat([outputIMG, frameCropped])
         else:
             frameCropped = frame[0:h, w1:w2]
             cv2.imwrite("working_files/output_image/output.png", frameCropped)
-            if blend == True:
-                cv2.imwrite("working_files/output_image/previousframe-0.png", frameCropped)
         if ret is False:
             print("done!")
             break
-        w1 += k
-        w2 += k
     except: 
         print("uh oh")
->>>>>>> parent of 8290bfd... working on blending
         break
     w1 += k
     w2 += k
-    # except: 
-    #     print("uh oh")
-    #     break
     count += 1
     print(count, "of", total)  
 try: cv2.imwrite("working_files/output_image/output.png", frameConjoined)
